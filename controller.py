8cfb42b890b0c0871dc14723f132a38894ce4835
Tomas Emanuel <temanuel@fi.uba.ar>
2023-11-16 18:24:24 -0300

    Incluyo handling de rules, sin correr

--- controller.py
+++ controller.py
@@ -6,7 +6,8 @@ from pox.pox.lib.addresses import EthAddr
 from collections import namedtuple 
 import os 
 import yaml
-
+import pox.lib.packet as pkt
+from pox.lib.addresses import IPAddr
 log = core.getLogger() 
 
 class Firewall(EventMixin):
@@ -20,25 +21,79 @@ class Firewall(EventMixin):
         self.firewall_rules = rules['rules']
         
         log.debug("Firewall Module Enabled")
+
+        self.switch = {
+                'dst_port': handle_dst_src_port,
+                'src_port': handle_dst_src_port,
+                'net_protocol': handle_net_protocol,
+                'src_mac': handle_src_mac,
+                'dst_mac': handle_dst_mac,
+                'src_ip': handle_dst_src_ip,
+
+        }
             
    
     def _handle_connection_up(self, event):
+        print()
         if event.dpid == self.firewall_switch:
             log.debug("Firewall Switch Connected: %s", dpidToStr(event.dpid))
             for rule in self.firewall_rules:
                 self._install_rule(event, rule)
     
     def _install_rule(self, event, rule):
-        # TODO: Implement the function that installs the given rule in the switch
+
         block_match = of.ofp_match() # It’s used to create a match object that defines the criteria for OpenFlow rules.
-        
+        self._add_rule(block_match, rule) # Adds the rule to the block_match object.
+
         msg = of.ofp_flow_mod()
         msg.match = block_match # Assigns the customized block_match to the match field of a flow mode message.
                                 #  This message is then sent to the switch to update its flow table.
         event.connection.send(msg)
-        pass
+    
+    def _add_rule(self, block_match, rule):
+        for key in rule.keys():
+            self.switch.get(key, lambda x, block_match=None: print(f"Unhandled key: {x} with block_match: {block_match}"))(rule[key],key, block_match)
 
+
+    def handle_dst_src_port(value,key, block_match):
+        if "src_port" == key:
+            block_match.tp_src = IPAddr(value)
+        if "dst_port" == key:
+            block_match.tp_dst = IPAddr(value)
+        print(f"Handling dst_port: {value} with block_match: {block_match}")
+    
+    def handle_dst_src_ip(value,key, block_match):
+        if "src_ip" == key:
+            block_match.nw_src = value
+        if "dst_ip" == key:
+            block_match.nw_dst = value
+        print(f"Handling dst_ip or src_ip with : {value} with block_match: {block_match}")
+    
+    def handle_net_protocol(value,key, block_match):
+        protocols = {
+                "TCP": pkt.ipv4.TCP_PROTOCOL,
+                "UDP": pkt.ipv4.UDP_PROTOCOL,
+                "ICMP": pkt.ipv4.ICMP_PROTOCOL,
+            }
+        block_match.nw_proto = protocols[value]
+
+
+    def handle_dst_src_mac(value,key, block_match):
+
+        if "src_mac" == key:
+                block_match.dl_src = EthAddr(value)
+        if "dst_mac" == key:
+                block_match.dl_dst = EthAddr(value)
+        print(f"Handling src_mac or dst_mac: {value} with block_match: {block_match}")
+        # Your logic for 'src_mac'
+
+        
     
     def launch(): 
         # Starting the Firewall module
         core.registerNew(Firewall)
+
+
+def main():
+    firewall = Firewall()
+    firewall._handle_connection_up()
